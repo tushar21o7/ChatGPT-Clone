4e6b8d24ad9c04b2f5ac9886f3bbf5ef11fb0776
tusharmishra969@gmail.com <tusharmishra969@gmail.com>
2025-09-08 04:09:28 +0530

    fix error

--- src/app/api/chats/[id]/route.ts
+++ src/app/api/chats/[id]/route.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 
 export async function POST(
     req: Request,
-    { params }: { params: { id: string } }
+    { params }: { params: Promise<{ id: string }> }
 ) {
     const { id } = await params;
 
